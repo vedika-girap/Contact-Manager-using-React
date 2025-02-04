31814aa3af5a24a412a1f8c9142e199501a99cef
vedika-girap <vedikalgirap@gmail.com>
2025-02-04 21:17:39 +0530

    Initial commit

---
+++ src/components/AddContact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AddContact extends React.Component{
+    render(){
+        return(
+            <div className="ui main">
+                <h2>Add Contact</h2>
+                <form className="ui form">
+
+                </form>
+            </div>
+        )
+        
+    }
+}
